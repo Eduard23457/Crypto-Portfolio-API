15070e89b002c013f567b5f32e03cdbb7c87efb4
Эдуард <khidiyatov2000@bk.ru>
2025-08-15 00:34:02 +0500

    Initial commit

---
+++ src/main/java/org/example/cryptoportfolioapi/CryptoPortfolioApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.cryptoportfolioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoPortfolioApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoPortfolioApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cryptoportfolioapi/CryptoPortfolioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cryptoportfolioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoPortfolioApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
